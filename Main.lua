7edd9a0f7b78bad7c69f5bb9b76d000b11616725
divinedivie <idontshipthisjalbert@gmail.com>
2026-09-05 11:24:21 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/scripts-api-true/1b5cc4e168b88fe9d992d46bf55df7c8/raw/cc966714809d1b4c3d5e6162cb04a120b59e4438/ALLINONE.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/scripts-api-true/eb53a600a8c1c11fd41bc2e97068b856/raw/4ff857b9a9d699de2732c424af03438a1fbc84eb/ALLINONE.lua"))()
